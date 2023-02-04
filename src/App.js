f1f536049ce75561c4c382886e22e8945eddfdbe
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-04 15:22:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-4xl font-bold underline">This is big big header</h1>
+      <p className='text-3xl underline font-thin'>This is just a paragraph</p>
     </div>
   );
 }
