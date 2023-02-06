f00b79337544ce337b0e9ef0ecdd698702742519
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-06 09:30:27 +0600

    3rd  added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar className="text-4xl "></NavBar>
       <h1 className="text-4xl font-bold underline">This is big big header</h1>
       <p className=' underline font-thin'>This is just a paragraph</p>
     </div>
