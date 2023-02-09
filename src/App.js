e9c93b45a0bdbd77daf6ef3fe4177af9b9288a31
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-09 16:01:23 +0600

    10th new  added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <h1 className="text-4xl font-bold underline">This is big big header</h1>
       <p className=' underline font-thin'>This is just a paragraph</p>
       <Pricing></Pricing>
-      
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
---
+++ src/components/PhoneBar/PhoneBar.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PhoneBar = () => {
+    const [phone, setPhone]= useState([])
+    useEffect( ()=> {
+        // fetch(' https://openapi.programming-hero.com/api/phones?search=iphone')
+        // then(res=> res.json())
+        // then(data => {})
+        
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PhoneBar;
