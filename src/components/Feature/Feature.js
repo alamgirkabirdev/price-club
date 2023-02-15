f7c794e2ed99618dd4c9a6a1fae1615fa551a391
Alamgir kabir <alamgirkabir.dev@gmail.com>
2023-02-15 21:43:09 +0600

    12th new  added

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -3,7 +3,7 @@ import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 const Feature = ({feature}) => {
     return (
-        <div className=''flex item-cencter>
+        <div className='flex items-center gap-4 mb-2'>
             <CheckCircleIcon className="h-6 w-6  text-green-500"/>
             <p className=' h-6 '>{feature}</p>
         </div>
--- src/components/PhoneBar/PhoneBar.js
+++ src/components/PhoneBar/PhoneBar.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const PhoneBar = () => {
-    const [phone, setPhone]= useState([])
+    const [phones, setPhones]= useState([]);
     useEffect( ()=> {
         // fetch(' https://openapi.programming-hero.com/api/phones?search=iphone')
-        // .then(res=> res.json())
+        // .then(res => res.json())
         // .then(data => {})
 
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data => {
-            const phonesLoaded = data.data.data;
-            console.log(phonesLoaded);
+            const phones = data.data.data;
+            console.log(phones);
         });
 
     }, [])
